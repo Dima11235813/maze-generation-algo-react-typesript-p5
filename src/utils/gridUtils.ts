b873e0ecd6007cd932067efeb21aa6653f46f713
Dmitri <LarionovDmitri@gmail.com>
2019-09-28 19:18:15 -0500

    added grid util which gets the index of each neihbor cell based on current cell

---
+++ src/utils/gridUtils.ts
@@ -0,0 +1,20 @@
+export const getPointValsAtIndex = (
+    row: number,
+    col: number,
+    numColumns: number,
+    numRows: number
+) => {
+    debugger
+    //Handle the (literal) edge cases
+    if (
+        //in case index of neighbors would be outsidd of the grid
+        row < 0 ||
+        col < 0 ||
+        col > numColumns ||
+        row > numRows
+    ) {
+        return -1
+    } else {
+        return row * col * numColumns
+    }
+}
