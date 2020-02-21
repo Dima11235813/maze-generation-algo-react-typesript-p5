2af125a7779f2c8bffbe8805a45957fe5c7e1e39
Dmitri <LarionovDmitri@gmail.com>
2020-02-20 20:52:50 -0600

    added placeholder for footer

---
+++ src/views/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer(props: any) {
+  const footerStyles = {
+    backgroundColor: "black",
+    minHeight: "20px"
+  };
+
+  return <div style={footerStyles}></div>;
+}
